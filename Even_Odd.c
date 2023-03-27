391dc3cec4f84734c355f23643ac2afd58670bb2
siboniseni <siboniseni.kasa@outlook.com>
2023-03-27 14:31:19 -0700

    File modified

--- Even_Odd.c
+++ Even_Odd.c
@@ -2,28 +2,33 @@
 
 //This program will take a value and determine if 
 //its an Even or Odd number
+//Does not take negative values
 
 
 int main() {
 
-    int n, x;
+    int n = 0, x;
     
-    printf("Enter a value: %d", n);
+    printf("Enter a value: %d\n", n);
     scanf("%d", &n);
     
     x = n % 2;
     
-    if (x == 0)
+    if (x == 0 & n > 0)
     {
         printf("%d is an Even number.", n);
     }
-    else if (x != 0)
+    else if (n < 0)
+    {
+        printf("Enter positive numbers only!");
+    }
+    else if (n != 0)
     {
         printf("%d is an Odd number.", n);
     }
     else 
     {
-        printf("%d is zero.", n);
+        printf("In mathematics zero is considered an Even number.");
     }
     
     return 0;
