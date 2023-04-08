754f29d628d0ccb888c88cb34e64a7d86b6bdd50
siboniseni <siboniseni.kasa@outlook.com>
2023-04-07 22:53:56 -0700

    Even numbers

--- Even_numbers.c
+++ Even_numbers.c
@@ -9,8 +9,8 @@ int main()
 {
     int x;
     
-   for (x=1; x<=20; x++)
-   {
+   for (x = 1; x <= 20; x++) //for (i = 0; i <= 10; i = i + 2)
+   {				// Easiest way to print out even numbers
        if ((x%2) == 0)
        {
            printf("%i\n", x );
